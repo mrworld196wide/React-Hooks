8204f3dbfc9e3da0dcf07081a6c97261dda5762b
mrworld196wide <shubhum19@gmail.com>
2023-08-07 09:40:24 +0530

    addd all files

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import UseState from "./components/usestate/UseState";
+import UseStateObject from "./components/usestate/UseStateObject";
+import UseEffect from "./components/useeffect/UseEffect";
+import ClearUp from "./components/useeffect/ClearnUp";
+// useContext file below
+import Parent from "./Parent";
+import ReducerHook from "./components/usereducer/ReducerHook";
+import RefHook1 from "./components/useref/RefHook1";
+import RefHook from "./components/useref/RefHook";
+import LayoutEffect from "./components/uselayouteffect/LayoutEffect";
+import MemoHook from "./components/usememo/MemoHook";
+import Callbackhook from "./components/usecallback/CallbackHook";
+
+const App = () => {
+  return <Parent />;
+};
+
+export default App;
---
+++ src/components/usecontext/userContext.jsx
@@ -0,0 +1,19 @@
+import { createContext, useContext } from "react";
+
+const AppContext = createContext();
+
+const AppProvider = ({ children }) => {
+  const userData = {
+    name: "Aishwar",
+    age: 21,
+  };
+
+  return <AppContext.Provider value={userData}>{children}</AppContext.Provider>;
+};
+
+// custom hook
+const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider, useGlobalContext };
